64714c08f9e0eb0d05e1c31956bf014ac6224586
Dennis Rasmussen <dennis@deras.dk>
2016-03-27 13:04:52 +0200

    Encryption of string

---
+++ Rodekassen/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rodekassen
+{
+    public class Class1
+    {
+    }
+}
